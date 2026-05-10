753519536a195afcf14a16c074ad07db0914bf8e
Murian <y2k001hj@gmail.com>
2026-05-11 07:17:47 +0900

    Update const.py

--- custom_components/korea_gasapp/const.py
+++ custom_components/korea_gasapp/const.py
@@ -16,16 +16,21 @@ CONF_OS_VERSION = "os_version"
 CONF_PLATFORM = "platform"
 CONF_POLL_INTERVAL = "poll_interval"
 CONF_READING_ENTITY_ID = "reading_entity_id"
+CONF_READING_ROUND = "reading_round"
 CONF_SUBMIT_DAY = "submit_day"
 CONF_SUBMIT_TIME = "submit_time"
 CONF_TID = "tid"
 CONF_USER_AGENT = "user_agent"
 CONF_USE_CONTRACT_NUM = "use_contract_num"
 
+READING_ROUND_UP = "ceil"
+READING_ROUND_DOWN = "floor"
+
 DEFAULT_POLL_INTERVAL = 60
 DEFAULT_MAX_READING_DELTA = 500
 DEFAULT_SUBMIT_DAY = 5
 DEFAULT_SUBMIT_TIME = "08:00:00"
+DEFAULT_READING_ROUND = READING_ROUND_UP
 
 DEFAULT_API_BASE_URL = "https://app.gasapp.co.kr/api/"
 DEFAULT_APP_PLATFORM = "android"
